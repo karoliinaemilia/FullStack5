f49617b62a8dc193b2696b3cabe888427f7a1e95
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-21 19:51:55 +0300

    5.5-5.6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Togglable from './components/Togglable'
 
 class App extends React.Component {
   constructor(props) {
@@ -98,7 +99,9 @@ class App extends React.Component {
           {this.state.blogs.map(blog => 
             <Blog key={blog._id} blog={blog}/>
           )}
+        <br/>
         <div>
+        <Togglable buttonLabel='new blog' ref={component => this.a = component}>
           <h3>create new</h3>
           <form onSubmit={this.addBlog}>
           <div>
@@ -127,6 +130,7 @@ class App extends React.Component {
           </div>
           <button type="submit">create</button>
           </form>
+        </Togglable>
         </div>
       </div>
     )
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,46 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingBottom: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    const blog = this.props.blog
+
+    const showWhenVisible = { display: this.state.visible ? '' : 'none'}
+    
+    return (
+      <div style={blogStyle}>
+        <div onClick={this.toggleVisibility} style={{cursor: 'pointer'}}>
+          {blog.title} {blog.author}
+        </div>
+        <div style={showWhenVisible}>
+          &nbsp; <a href={blog.url}>{blog.url}</a>
+          <br/>
+          &nbsp; {blog.likes} likes &nbsp; <button onClick={(() => {})}>like</button>
+          <br/>
+          &nbsp; added by {blog.username}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
