42a7b205339e7dfff17e3c0c38fb4e009952f6e3
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-23 19:36:08 +0300

    5.13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -18,4 +18,22 @@ describe.only('<SimpleBlog />', () => {
     expect(namesDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain(blog.likes)
   })
+
+  it('clicking the button twice calls event handler twice', () => {
+    const blog = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 10
+    }
+
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+    
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
